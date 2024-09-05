0472a5f575e33ea54e5b1e26e7eceef1a338edf2
Samarth-Koundal <samarthkoundal121121@gmail.com>
2024-09-05 16:33:41 +0530

    Add files via upload

---
+++ portfolio website/home page/home-script.js
@@ -0,0 +1,13 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let options = {
+      strings: ["Developer", "Designer", "Freelancer"],
+      typeSpeed: 100,
+      loop: true
+  };
+
+  let typed = new Typed("#type-it", options);
+});
+  
